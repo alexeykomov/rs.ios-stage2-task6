c63007bfaa3cc34a0658a7b3f00c49a24da47ed3
alexeykomov <alexeykofficial@gmail.com>
2020-06-19 23:34:02 +0300

    add: inital screen is similar to wireframe

---
+++ RsSchoolTask2.6/RsSchoolTask2.6/GalleryViewController.h
@@ -0,0 +1,17 @@
+//
+//  GalleryViewController.h
+//  RsSchoolTask2.6
+//
+//  Created by Alex K on 6/19/20.
+//  Copyright © 2020 Alex K. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GalleryViewController : UICollectionViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
--- RsSchoolTask2.6/RsSchoolTask2.6/StartScreenViewController.m
+++ RsSchoolTask2.6/RsSchoolTask2.6/StartScreenViewController.m
@@ -11,6 +11,9 @@
 
 @interface StartScreenViewController ()
 @property (weak, nonatomic) IBOutlet UIButton *buttonStart;
+@property (weak, nonatomic) IBOutlet UILabel *labelGreetings;
+@property (strong, nonatomic) UIStackView *shapesContainer;
+
 
 @end
 
@@ -22,6 +25,38 @@
     
     self.buttonStart.layer.cornerRadius = 55.0 / 2.0;
     self.buttonStart.backgroundColor = UIColor.yellowColor;
+    
+    UIView *circle = [[UIView alloc] init];
+    [circle.widthAnchor constraintEqualToConstant:70.0].active = true;
+    [circle.heightAnchor constraintEqualToConstant:70.0].active = true;
+    UIView *square = [[UIView alloc] init];
+    [square.widthAnchor constraintEqualToConstant:70.0].active = true;
+    [square.heightAnchor constraintEqualToConstant:70.0].active = true;
+    UIView *triangle = [[UIView alloc] init];
+    [triangle.widthAnchor constraintEqualToConstant:70.0].active = true;
+    [triangle.heightAnchor constraintEqualToConstant:70.0].active = true;
+    
+    circle.backgroundColor = UIColor.redColor;
+    square.backgroundColor = UIColor.blueColor;
+    triangle.backgroundColor = UIColor.greenColor;
+    
+    self.shapesContainer = [[UIStackView alloc] init];
+    [self.view addSubview:self.shapesContainer];
+    self.shapesContainer.translatesAutoresizingMaskIntoConstraints = false;
+    
+    self.shapesContainer.backgroundColor = UIColor.redColor;
+    self.shapesContainer.axis = UILayoutConstraintAxisHorizontal;
+    self.shapesContainer.alignment = UIStackViewAlignmentCenter;
+    self.shapesContainer.distribution = UIStackViewDistributionEqualCentering;
+    self.shapesContainer.spacing = 20.0;
+    
+    [self.shapesContainer addArrangedSubview:circle];
+    [self.shapesContainer addArrangedSubview:square];
+    [self.shapesContainer addArrangedSubview:triangle];
+    
+    [[self.shapesContainer topAnchor] constraintEqualToAnchor:[self.labelGreetings bottomAnchor] constant:115].active = true;
+    [[self.shapesContainer centerXAnchor] constraintEqualToAnchor:[self.view centerXAnchor]].active = true;
+    
     
     NSLog(@"View loaded");
 }
