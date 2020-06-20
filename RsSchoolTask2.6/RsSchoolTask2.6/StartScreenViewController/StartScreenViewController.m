47687784657dd791eb688d27f76d0fe530c06e94
alexeykomov <alexeykofficial@gmail.com>
2020-06-20 15:40:46 +0300

    fix: layout for small screens

--- RsSchoolTask2.6/RsSchoolTask2.6/StartScreenViewController/StartScreenViewController.m
+++ RsSchoolTask2.6/RsSchoolTask2.6/StartScreenViewController/StartScreenViewController.m
@@ -31,6 +31,25 @@
     
     self.buttonStart.layer.cornerRadius = 55.0 / 2.0;
     self.buttonStart.backgroundColor = UIColor.yellowColor;
+    self.buttonStart.translatesAutoresizingMaskIntoConstraints = false;
+    [self.buttonStart.widthAnchor constraintEqualToConstant:250].active = true;
+    [self.buttonStart.heightAnchor constraintEqualToConstant:55].active = true;
+    [[self.buttonStart centerXAnchor] constraintEqualToAnchor:[self.view centerXAnchor]].active = true;
+    [[self.buttonStart bottomAnchor] constraintLessThanOrEqualToAnchor:[self.view bottomAnchor] constant:-10].active = true;
+    NSLayoutConstraint *buttonStartDesiredBottom = [[self.buttonStart bottomAnchor] constraintEqualToAnchor:[self.view bottomAnchor] constant:-100];
+    buttonStartDesiredBottom.active = true;
+    buttonStartDesiredBottom.priority = 900.0;
+    
+
+    self.labelGreetings.translatesAutoresizingMaskIntoConstraints = false;
+    [[self.labelGreetings centerXAnchor] constraintEqualToAnchor:[self.view centerXAnchor]].active = true;
+    NSLayoutConstraint *labelGreetingsDesiredTop = [[self.labelGreetings topAnchor] constraintEqualToAnchor:[self.view topAnchor] constant:115];
+    labelGreetingsDesiredTop.active = true;
+    labelGreetingsDesiredTop.priority = 900.0;
+    [self.labelGreetings.heightAnchor constraintEqualToConstant:55].active = true;
+    [[self.labelGreetings topAnchor] constraintGreaterThanOrEqualToAnchor:[self.view topAnchor] constant:10].active = true;
+    
+
     
     Circle *circle = [[Circle alloc] initWithColor:colors.red];
     [circle.widthAnchor constraintEqualToConstant:70.0].active = true;
@@ -56,7 +75,12 @@
     [self.shapesContainer addArrangedSubview:square];
     [self.shapesContainer addArrangedSubview:triangle];
     
-    [[self.shapesContainer topAnchor] constraintEqualToAnchor:[self.labelGreetings bottomAnchor] constant:115].active = true;
+    [self.shapesContainer.heightAnchor constraintEqualToConstant:70.0].active = true;
+    NSLayoutConstraint *desiredDistanceFromLabelGreetings = [[self.shapesContainer topAnchor] constraintEqualToAnchor:[self.labelGreetings bottomAnchor] constant:115];
+    desiredDistanceFromLabelGreetings.active = true;
+    desiredDistanceFromLabelGreetings.priority = 900.0;
+    [[self.shapesContainer topAnchor] constraintGreaterThanOrEqualToAnchor:[self.labelGreetings bottomAnchor] constant:50].active = true;
+    [[self.shapesContainer bottomAnchor] constraintLessThanOrEqualToAnchor:[self.buttonStart topAnchor] constant:-70].active = true;
     [[self.shapesContainer centerXAnchor] constraintEqualToAnchor:[self.view centerXAnchor]].active = true;
     
     self.labelGreetings.textColor = colors.black;
